cedc0b9cf6bc1410f84424932d5d04ea8064146b
HenriqueBMa <92217058+HenriqueBMa@users.noreply.github.com>
2022-09-01 08:08:12 -0300

    Working alegrete

--- alegrete.py
+++ alegrete.py
@@ -9,7 +9,11 @@ def compute_mse(theta_0, theta_1, data):
     :param data: np.array - matriz com o conjunto de dados, x na coluna 0 e y na coluna 1
     :return: float - o erro quadratico medio
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    accumulated_error = 0 # Cria uma variavel para guardar a soma dos erros
+    for size, price in data: # Dado cada tamanho de propriedade e preço no banco de dados
+        error = (price - (theta_0 + theta_1*size))  # Determina o erro
+        accumulated_error += error*error/len(data)  # Soma o erro ao acumulador
+    return accumulated_error # Retorna o erro somado
 
 
 def step_gradient(theta_0, theta_1, data, alpha):
@@ -21,7 +25,18 @@ def step_gradient(theta_0, theta_1, data, alpha):
     :param alpha: float - taxa de aprendizado (a.k.a. tamanho do passo)
     :return: float,float - os novos valores de theta_0 e theta_1, respectivamente
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    accumulated_error0 = 0  # Cria variaveis para guardar a soma dos erros usada para calcular o gradiente
+    accumulated_error1 = 0
+
+    for size, price in data:
+        error = (theta_0 + theta_1 * size - price)/len(data) # Calcula o erro produzido pelos thetas atuais (e ja divide pelo tamanho da base de dados para que o resultado não seja um número muito grande)
+        accumulated_error0 += error  # Pega parte do valor do gradiente de theta 0
+        accumulated_error1 += error*size  # Pega parte do valor do gradiente de theta 1
+
+    new_theta_0 = theta_0 - alpha*2*accumulated_error0  # Calcula o novo valor de theta 0 a partir do seu gradiente
+    new_theta_1 = theta_1 - alpha*2*accumulated_error1  # Calcula o novo valor de theta 1 a partir do seu gradiente
+
+    return new_theta_0, new_theta_1  # Devolve os novos valores de theta 0 e 1
 
 
 def fit(data, theta_0, theta_1, alpha, num_iterations):
@@ -39,4 +54,30 @@ def fit(data, theta_0, theta_1, alpha, num_iterations):
     :param num_iterations: int - numero de épocas/iterações para executar a descida de gradiente
     :return: list,list - uma lista com os theta_0 e outra com os theta_1 obtidos ao longo da execução
     """
-    raise NotImplementedError  # substituir pelo seu codigo
+    theta_0_list = [theta_0] # Cria a lista de valores de theta 0
+    theta_1_list = [theta_1] # Cria a lista de valores de theta 1
+
+    for iteration in range(num_iterations): # Progride uma iteração até alcançar o numero de iterações pedido
+
+        theta_0, theta_1 = step_gradient(theta_0, theta_1, data, alpha)  # Executa a descida de gradiente e salva os novos valores de theta 0 e 1
+
+        theta_0_list.append(theta_0)  # Coloca o novo valor de theta 0 na lista
+        theta_1_list.append(theta_1)  # Coloca o novo valor de theta 1 na lista
+
+    return theta_0_list, theta_1_list  # Retorna as duas listas
+
+if __name__ == '__main__':
+    data = np.genfromtxt('alegrete.csv', delimiter=',')
+    fit(data, -3, 6, 0.01, 90)
+    '''
+    Melhor solução:
+    
+    theta_0 = -3
+    theta_1 = 6
+    alpha = 0.01
+    interactions = 90
+    fit(data,-3,6,0.01,90)
+    
+    finishing thetas:
+    -3.471544264398201, 1.1627591930896441
+    '''
